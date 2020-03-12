c7d264d01c6450ddc39663fe7822ec09f8c2d67c
Takaf19 <tk.f19ms@gmail.com>
2020-03-12 13:48:23 +0900

    foutes.rb 修正
    users のshowアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
   resources :products, only: [:new, :show]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show, :new, :create]
 end
