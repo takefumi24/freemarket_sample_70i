b80e9b7ee90dda73ebe258a97b4e8b87f3718099
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-03-12 19:24:18 +0900

    購入ボタンの条件分岐

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
   belongs_to :product, optional: true
+
+  # 画像アップロード実装後
   # mount_uploaders :image_url, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "products#index"
-  resources :products, only: [:new, :create, :show]
+  resources :products, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:new, :create]
   resources :categories, only: [:index, :show]
 end
