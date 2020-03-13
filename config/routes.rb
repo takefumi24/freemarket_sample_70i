04152d0180ab40fcb20bdf13aba8a941479a541b
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-03-13 13:21:22 +0900

    create-buy-confirmation-view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
 
-  resources :products, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :products, only: [:new, :create, :show, :edit, :update, :destroy] do
+    collection do
+      get 'buy'
+    end
+  end
+
   resources :users, only: [:show, :new, :create]
 
   resources :categories, only: [:index, :show]
